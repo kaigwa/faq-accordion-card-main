7448367419a6000a7b94fbe898cf04594b73741d
Reset <kaigwaedwin@gmail.com>
2023-05-04 11:39:43 +0300

    new features

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
-    this.classList.toggle("active");
+    // this.classList.toggle("active");
 
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
@@ -14,5 +14,9 @@ for (i = 0; i < acc.length; i++) {
     } else {
       panel.style.display = "block";
     }
+    // console.log(this);
+
   });
 }
+
+
