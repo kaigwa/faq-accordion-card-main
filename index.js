30c83469f0e5d6e8d301b4c207de756001580782
Reset <kaigwaedwin@gmail.com>
2023-05-11 00:18:26 +0300

    new features

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 // declare function all question 'class' selector;
 const questions = document.querySelectorAll('.question');
+// const panels = document.querySelectorAll('.panel');
 
 // for each question button clicked add event listener function;
 questions.forEach(question => {
-  question.addEventListener('click', () => {
+  question.addEventListener('click', (e) => {
    
+    
     // click fuction has to switch to activate the 'active class';
     question.classList.toggle('active')
   })
 })
 
 
+// panels.forEach((acc) => {
+//   if (
+//     e.target.nextElementSibling !== acc &&
+//     acc.classList.contains('active')
+//   ) {
+//     acc.classList.remove('active');
+   
+//   }
+// });
