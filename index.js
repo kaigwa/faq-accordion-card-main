f1b7eb01a4a3cecb385c9fc1f7377eddb0773745
Reset <kaigwaedwin@gmail.com>
2023-05-12 07:13:43 +0300

    new features

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-// declare function all question 'class' selector;
+// get all accordion questions through 'class' selector;
 const questions = document.querySelectorAll('.question');
-// const panels = document.querySelectorAll('.panel');
+const panels = document.querySelectorAll('.panel');
 
 // for each question button clicked add event listener function;
 questions.forEach(question => {
-  question.addEventListener('click', (e) => {
+  question.addEventListener('click', () => {
+      
    
-    
-    // click fuction has to switch to activate the 'active class';
-    question.classList.toggle('active')
+    // click function has to switch to activate the 'active class';
+    question.classList.toggle('active');
   })
 })
 
 
-// panels.forEach((acc) => {
-//   if (
-//     e.target.nextElementSibling !== acc &&
-//     acc.classList.contains('active')
-//   ) {
-//     acc.classList.remove('active');
-   
-//   }
-// });
+
+
+
+
+
